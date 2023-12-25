704d0f596dc07eae427777f75f39a0c78f4c376d
t99forever <ybezema@gmail.com>
2023-12-25 18:02:48 +0100

    Added SQL table to CSV table conversion script

---
+++ general/python/mysql-csv_conversion.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import URL
+
+# CONNECT TO NEW SQL DATABASE THROUGH SQL ALCHEMY
+url_object = URL.create(
+   "mysql+pymysql",
+   host = "localhost",
+   username = "default",
+   password = "default",
+   database = "autotests",
+   )
+
+engine = create_engine(
+   url_object
+   )
+
+# RACE
+df = pd.read_sql("SELECT * FROM autotests_full", 
+                 con = engine
+                 )
+
+df.to_csv("csv_template.csv",
+          sep = ";"
+          )
