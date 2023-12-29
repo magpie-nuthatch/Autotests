964c18f7aeab43332800ef44309be7a5021377a3
t99forever <ybezema@gmail.com>
2023-12-29 17:57:14 +0100

    Created bar graphs

--- analysis/projects/aandrijving_brandstof-land_continent/visual_stats.r
+++ analysis/projects/aandrijving_brandstof-land_continent/visual_stats.r
@@ -18,6 +18,80 @@ land <- df$land
 brandstof <- df$brandstof
 aandrijving <- df$aandrijving
 
-barplot <- plot(x = continent, type = h)
+barchart_count_continent <- ggplot(data = df,
+                                   aes(x = reorder(continent, continent,
+                                                   function(x) - length(x)))
+) + 
+  geom_bar(color = "darkred",
+           fill = "pink",
+           linewidth = 1,
+           lineend = "square",
+           na.rm = TRUE,
+           show.legend = FALSE
+) +
+  labs(title = "Hoeveelheid geteste auto's per continent",
+       subtitle = "Auto's getest in tijdschriften, 2022 en 2023",
+       caption = "AutoVisie, 2022/2023",
+       x = "Continent",
+       y = "Hoeveelheid geteste auto's",
+)
 
-barplot
+barchart_count_land <- ggplot(data = df,
+                              aes(x = reorder(land, land,
+                                              function(x) - length(x)))
+) + 
+  geom_bar(color = "darkred",
+           fill = "pink",
+           linewidth = 1,
+           lineend = "square",
+           na.rm = TRUE,
+           show.legend = FALSE
+  ) +
+  labs(title = "Hoeveelheid geteste auto's per land",
+       subtitle = "Auto's getest in tijdschriften, 2022 en 2023",
+       caption = "AutoVisie, 2022/2023",
+       x = "Land",
+       y = "Hoeveelheid geteste auto's",
+  )
+
+barchart_count_continent
+barchart_count_land
+
+barchart_count_brandstof <- ggplot(data = df,
+                                   aes(x = reorder(brandstof, brandstof,
+                                                   function(x) - length(x)))
+) + 
+  geom_bar(color = "darkblue",
+           fill = "lightblue",
+           linewidth = 1,
+           lineend = "square",
+           na.rm = TRUE,
+           show.legend = FALSE
+  ) +
+  labs(title = "Hoeveelheid geteste auto's per type brandstof",
+       subtitle = "Auto's getest in tijdschriften, 2022 en 2023",
+       caption = "AutoVisie, 2022/2023",
+       x = "Type brandstof",
+       y = "Hoeveelheid geteste auto's",
+  )
+
+barchart_count_aandrijving <- ggplot(data = df,
+                              aes(x = reorder(aandrijving, aandrijving,
+                                              function(x) - length(x)))
+) + 
+  geom_bar(color = "darkblue",
+           fill = "lightblue",
+           linewidth = 1,
+           lineend = "square",
+           na.rm = TRUE,
+           show.legend = FALSE
+  ) +
+  labs(title = "Hoeveelheid geteste auto's per type aandrijving",
+       subtitle = "Auto's getest in tijdschriften, 2022 en 2023",
+       caption = "AutoVisie, 2022/2023",
+       x = "Type aandrijving",
+       y = "Hoeveelheid geteste auto's",
+  )
+
+barchart_count_brandstof
+barchart_count_aandrijving
