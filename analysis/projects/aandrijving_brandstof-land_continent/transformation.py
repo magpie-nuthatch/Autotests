6a9cb775573a14cd11e7bb091446e206b29f4e4d
t99forever <ybezema@gmail.com>
2023-12-30 12:16:01 +0100

    Reworked descriptives analysis in transformation file

--- analysis/projects/aandrijving_brandstof-land_continent/transformation.py
+++ analysis/projects/aandrijving_brandstof-land_continent/transformation.py
@@ -1,31 +1,13 @@
 import pandas as pd
 import numpy as np
+from dataframe_descriptives_normality import descriptives 
 
 df = pd.read_csv("general\\csv\\transformed\\autotests_trans-initial.csv",
                  encoding='latin1',
                  sep = ";",
                  index_col = 0)
 
-def descriptives(df):
-    print()
-    print(f"Information on columns in current dataframe:")
-    print(df.info(verbose = True,
-                  show_counts = True))
-
-    for column in df:
-        df_loop = df.dropna(subset = column)
-        print()
-        print(f"Descriptives for column {column}:")
-        print(df_loop[column].describe())
-        if df_loop[column].dtype == "object" and len(df_loop[column].unique()) < 10: # Displays all unique values for columns with less than 10 unique values
-            unique_values = sorted(df_loop[column].unique())
-            print()
-            print(f"Unique values in column {column}:")
-            for value in unique_values:
-                print(value)
-        print()
-
-descriptives(df)
+descriptives(df, 5, 15)
 
 df_trans = df
 
@@ -39,7 +21,7 @@ df_trans.dropna(subset = ["continent", "land", "brandstof", "aandrijving"],
                 how = "any",
                 inplace = True)
 
-descriptives(df_trans)
+descriptives(df_trans, 5, 15)
 
 df_trans.to_csv("general\\csv\\transformed\\autotests_trans-aandrijving_brandstof.csv",
           sep=";")
